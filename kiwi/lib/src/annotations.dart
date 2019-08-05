5388e6a6918e41014e4704d50ac34b49f7eaba40
Andrey Babinov <5turman@mail.ru>
2019-08-05 20:57:17 +0300

    Replaced Register annotation by Inject and Named

--- kiwi/lib/src/annotations.dart
+++ kiwi/lib/src/annotations.dart
@@ -1,48 +1,13 @@
-/// An annotation that generates code for registering factories
-/// using the kiwi container.
-class Register {
-  /// Create an annotation that will generate a `registerFactory` method.
-  const Register.factory(
-    this.type, {
-    this.name,
-    this.from,
-    this.resolvers,
-    this.constructorName,
-  })  : assert(type != null),
-        oneTime = null;
+/// An annotation that marks the constructor for generating a factory.
+class Inject {
+  const Inject();
+}
 
-  /// Create an annotation that will generate a `registerSingleton` method.
-  const Register.singleton(
-    this.type, {
-    this.name,
-    this.from,
-    this.resolvers,
-    this.constructorName,
-  })  : assert(type != null),
-        oneTime = true;
+const inject = Inject();
 
-  /// The type to register.
-  final Type type;
+/// An annotation to specify the name of dependency in the constructor.
+class Named {
+  const Named(this.name) : assert(name != null);
 
-  /// The type to create when requesting [type].
-  final Type from;
-
-  /// The name under which the factory will be registered
-  ///
-  /// You must provide the same name in [Container.resolve]
-  /// to get the same factory.
   final String name;
-
-  /// A map that give for a type, the name under which it should be resolved
-  ///
-  /// For example if you have registered a type T under the name
-  /// 'myType', you have to specify it in this map in order
-  /// to use it instead of the default value for the type T.
-  final Map<Type, String> resolvers;
-
-  /// The name of the constructor to use inside the factory.
-  final String constructorName;
-
-  /// Whether the factory has to be created only one time.
-  final bool oneTime;
 }
